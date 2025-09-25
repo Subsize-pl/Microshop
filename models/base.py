d65d065345d1b00aed30e80736f9b9200a6cc01c
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-25 18:30:26 +0300

    Updated base.py in models package

---
+++ models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        name = cls.__name__.lower()
+        if name.endswith("y"):
+            return name[:-1] + "ies"
+        elif name.endswith("s"):
+            return name
+        else:
+            return name + "s"
