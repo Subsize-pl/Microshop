2a7b0613bb5f2914b41319badac4a13197e66fc4
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-25 11:05:33 +0300

    Updated DB settings

--- core/config.py
+++ core/config.py
@@ -2,17 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
+    db_host: str
+    db_port: int
+    db_user: str
+    db_password: str
+    db_name: str
+    db_echo: bool = False
 
     model_config = SettingsConfigDict(env_file=".env")
 
     @property
     def DB_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 
 settings = Settings()
