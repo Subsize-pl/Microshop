2139df15b2bc3eea1eeba852cef9b06c3ce8dd9e
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-27 23:31:57 +0300

    Add type aliases to product_helper

--- core/helpers/product_helper.py
+++ core/helpers/product_helper.py
@@ -1,3 +1,8 @@
+from typing import Annotated
+
+from pydantic import Field
+
+
 class ProductHelper:
     NAME_MAX_LEN = 128
     NAME_MIN_LEN = 1
@@ -7,6 +12,29 @@ class ProductHelper:
 
     PRICE_MAX_VALUE = None
     PRICE_MIN_VALUE = 1
+
+    NameStr = Annotated[
+        str,
+        Field(
+            max_length=NAME_MAX_LEN,
+            min_length=NAME_MIN_LEN,
+        ),
+    ]
+
+    DescriptionStr = Annotated[
+        str,
+        Field(
+            max_length=DESCRIPTION_MAX_LEN,
+            min_length=DESCRIPTION_MIN_LEN,
+        ),
+    ]
+
+    PriceInt = Annotated[
+        int,
+        Field(
+            ge=PRICE_MIN_VALUE,
+        ),
+    ]
 
 
 product_helper = ProductHelper()
