0371572390f34d371bc11114990435d9c47dc3c0
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-27 21:12:22 +0300

    Add products router (CR-options)

---
+++ products/routes.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+import products.crud as crud
+from core.helpers import db_helper
+from typing import List, Optional
+from .schemas import (
+    ProductCreate,
+    Product,
+)
+
+router = APIRouter(tags=["/products"])
+
+
+@router.get("/", response_model=List[Product])
+async def get_products(
+    session: AsyncSession = Depends(db_helper.session_dependency()),
+):
+    return await crud.get_products(session=session)
+
+
+@router.get("/{product_id}", response_model=Optional[Product])
+async def get_product(
+    product_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency()),
+):
+    result = await crud.get_product(session=session, product_id=product_id)
+    if result is not None:
+        return result
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {product_id} not found",
+    )
+
+
+@router.post("/")
+async def create_product(
+    product_in: ProductCreate,
+    session: AsyncSession = Depends(db_helper.session_dependency()),
+):
+    return await crud.create_product(session=session, product_in=product_in)
