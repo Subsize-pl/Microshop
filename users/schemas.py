d5a8517868d4cd6d06810f89577889ff93471f78
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-27 21:12:51 +0300

    Add users schemas

--- users/schemas.py
+++ users/schemas.py
@@ -1,11 +1,35 @@
 from .base import Base, NameStr
 from pydantic import Field, EmailStr
-from typing import Optional
+from typing import Optional, Annotated
 
 
-class UserCreate(Base):
+class UserBase(Base):
     firstname: NameStr
     surname: Optional[NameStr]
     username: NameStr
     email: EmailStr
     password: str
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class User(UserBase):
+    id: Annotated[int, Field(gt=1)]
+
+
+class UserDelete(User):
+    pass
+
+
+class UserUpdatePartial(UserBase):
+    firstname: Optional[NameStr] = None
+    surname: Optional[NameStr] = None
+    username: Optional[NameStr] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+
+class UserUpdate(UserBase):
+    pass
