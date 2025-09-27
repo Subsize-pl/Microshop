0173e1a4b33ce3ac14828cc8bca48ee55dc9c575
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-27 21:12:01 +0300

    Add products crud (CR-options)

---
+++ products/crud.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result
+from models.products import Product as ProductORM
+from typing import List, Optional
+from .schemas import (
+    Product,
+    ProductCreate,
+)
+
+
+async def get_products(session: AsyncSession) -> List[Product]:
+    stmt = select(Product).order_by(Product.id)
+    result: Result = await session.execute(stmt)
+    products = result.scalars().all()
+    return list(products)
+
+
+async def get_product(session: AsyncSession, product_id: int) -> Optional[Product]:
+    return await session.get(Product, product_id)
+
+
+async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
+    product = ProductORM(**product_in.model_dump())
+    session.add(product)
+    await session.commit()
+    await session.refresh(product)
+    return product
