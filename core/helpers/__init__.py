2da01dc0baca02ff5fec5a6f94bf1846f6b38b96
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-09-29 18:07:32 +0300

    Delete type aliases helper

--- core/helpers/__init__.py
+++ core/helpers/__init__.py
@@ -1,13 +1,11 @@
 __all__ = (
     "product_helper",
     "db_helper",
-    "type_aliases_helper",
     "user_helper",
     "pwd_helper",
 )
 
 from .product_helper import product_helper
 from .db_helper import db_helper
-from .type_aliases_helper import type_aliases_helper
 from .user_helper import user_helper
 from .password_helper import pwd_helper
