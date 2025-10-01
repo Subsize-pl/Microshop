b56f653f6e59df93a75d6beb518cf7dbb80599a8
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-10-01 22:20:05 +0300

    Add order mixin

--- core/mixins/__init__.py
+++ core/mixins/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "ProductRelationMixin",
     "UserRelationMixin",
     "AttributeRelationMixin",
+    "OrderRelationMixin",
 )
 from .timestamp import TimestampMixin
 from .category import CategoryRelationMixin
 from .product import ProductRelationMixin
 from .user import UserRelationMixin
 from .attribute import AttributeRelationMixin
+from .order import OrderRelationMixin
---
+++ core/mixins/order.py
@@ -0,0 +1,26 @@
+from typing import Optional, TYPE_CHECKING
+from core.helpers import TbHelper
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from models import Order
+
+
+class OrderRelationMixin:
+    _order_back_populates: Optional[str] = None
+    _order_nullable: bool = False
+
+    @declared_attr
+    def order_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey(f"{TbHelper.generate_tn("Order")}.id"),
+            nullable=cls._order_nullable,
+        )
+
+    @declared_attr
+    def attribute(cls) -> Mapped["Order"]:
+        return relationship(
+            "Order",
+            back_populates=cls._order_back_populates,
+        )
