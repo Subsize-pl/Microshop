dc92763981d7a8dc5832350164270b7fb97e9897
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-10-01 22:08:26 +0300

    Upgrade tablename helper

--- core/helpers/tablename.py
+++ core/helpers/tablename.py
@@ -1,8 +1,14 @@
 class TablenameHelper:
 
     @staticmethod
-    def generate_tn(model: str) -> str:
-        name = model.lower()
+    def generate_tn(model) -> str:
+        if isinstance(model, str):
+            name = model
+        else:
+            name = model.__name__
+
+        name = name.lower()
+
         if name.endswith("y"):
             return name[:-1] + "ies"
         elif name.endswith("s"):
