40ec36c29308d86f8d30aa13baad74e152adb68a
eduard plyushchevich <eduardplyushchevich@gmail.com>
2025-10-01 09:34:53 +0300

    Add category mixin

---
+++ core/mixins/category.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy.orm import relationship, Mapped, declared_attr, mapped_column
+from sqlalchemy import ForeignKey
+
+if TYPE_CHECKING:
+    from models.categories import Category
+
+
+class CategoryRelationMixin:
+    _category_back_populates: Optional[str] = None
+    _category_nullable: bool = False
+
+    @declared_attr
+    def category_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey(f"{Category.__tablename__}.id"),
+            nullable=cls._category_nullable,
+        )
+
+    @declared_attr
+    def category(cls) -> Mapped[Category]:
+        return relationship(
+            "Category",
+            back_populates=cls._category_back_populates,
+        )
