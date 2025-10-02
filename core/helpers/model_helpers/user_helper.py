c0fd6ec172e3cdf5354762560067b7b29c84f0a8
eduard plyushchevich <tachkafor@gmail.com>
2025-10-02 23:01:23 +0300

    Change user helper

--- core/helpers/model_helpers/user_helper.py
+++ core/helpers/model_helpers/user_helper.py
@@ -1,20 +1,30 @@
-from typing import Annotated
-
+from typing import Annotated, Optional
 from pydantic import Field
 
 
 class UserHelper:
-    FIRSTNAME_MAX_LEN = 128
-    FIRSTNAME_MIN_LEN = 1
+    FIRSTNAME_MAX_LEN: int = 128
+    FIRSTNAME_MIN_LEN: int = 1
+    FIRSTNAME_NULLABLE: bool = False
 
-    SURNAME_MAX_LEN = 128
-    SURNAME_MIN_LEN = 0
+    LASTNAME_MAX_LEN: int = 128
+    LASTNAME_MIN_LEN: int = 1
+    LASTNAME_NULLABLE: bool = False
 
-    USERNAME_MAX_LEN = 128
-    USERNAME_MIN_LEN = 1
+    USERNAME_MAX_LEN: int = 128
+    USERNAME_MIN_LEN: int = 1
+    USERNAME_NULLABLE: bool = False
+    USERNAME_UNIQUE: bool = True
+
+    EMAIL_MAX_LEN: int = 256  # RFC рекомендует до 254 символов
+    EMAIL_NULLABLE: bool = False
+    EMAIL_UNIQUE: bool = False
+
+    HASH_MAX_LEN: int = 128  # достаточно для bcrypt
+    HASH_NULLABLE: bool = False
 
     FirstnameStr = Annotated[
-        str,
+        Optional[str] if FIRSTNAME_NULLABLE else str,
         Field(
             min_length=FIRSTNAME_MIN_LEN,
             max_length=FIRSTNAME_MAX_LEN,
@@ -22,15 +32,15 @@ class UserHelper:
     ]
 
     SurnameStr = Annotated[
-        str,
+        Optional[str] if LASTNAME_NULLABLE else str,
         Field(
-            min_length=SURNAME_MIN_LEN,
-            max_length=SURNAME_MAX_LEN,
+            min_length=LASTNAME_MIN_LEN,
+            max_length=LASTNAME_MAX_LEN,
         ),
     ]
 
     UsernameStr = Annotated[
-        str,
+        Optional[str] if USERNAME_NULLABLE else str,
         Field(
             min_length=USERNAME_MIN_LEN,
             max_length=USERNAME_MAX_LEN,
