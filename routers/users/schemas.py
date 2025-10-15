38fa25e4058106ce56b433ebd9cfbe2b1e31ca85
eduard plyushchevich <tachkafor@gmail.com>
2025-10-15 21:17:45 +0300

    Change UserBase model and others

--- routers/users/schemas.py
+++ routers/users/schemas.py
@@ -1,31 +1,29 @@
 from datetime import datetime
-from typing import Annotated
-
-from black.brackets import Optional
+from typing import Annotated, Optional
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
-
 from core.helpers import user_helper
 
 
 class UserBase(BaseModel):
-    firstname: user_helper.FirstnameStr
-    surname: user_helper.SurnameStr
     username: user_helper.UsernameStr
-    email: EmailStr
-
-
-class User(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-    id: int
-    created_at: datetime
-    updated_at: datetime
-
-
-class UserCreate(UserBase):
     password: Annotated[
         str,
         Field(min_length=1),
     ]
+
+
+class UserCreate(UserBase):
+    firstname: user_helper.FirstnameStr
+    surname: user_helper.SurnameStr
+    email: EmailStr
+
+
+class User(UserBase, UserCreate):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
 
 
 class UserUpdate(UserBase):  # Partial update
@@ -33,6 +31,7 @@ class UserUpdate(UserBase):  # Partial update
     surname: Optional[user_helper.SurnameStr] = None
     username: Optional[user_helper.UsernameStr] = None
     email: Optional[EmailStr] = None
+    password: Optional[Annotated[str, Field(min_length=1)]] = None
 
 
 class UserDelete(UserBase):
