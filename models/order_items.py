608b98487ae32e95574e52fd11ec69c954942575
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-16 20:01:21 +0300

    Change order_items model

--- models/order_items.py
+++ models/order_items.py
@@ -1,3 +1,4 @@
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, declared_attr
 
 from core.mixins import OrderRelationMixin, ProductRelationMixin
@@ -6,12 +7,21 @@ from core.helpers import TbHelper
 
 
 class OrderItem(OrderRelationMixin, ProductRelationMixin, Base):
-    @declared_attr
-    def _order_back_populates(cls):
-        return TbHelper.generate_tn(cls)
+    # order - product association table
+    __tablename__ = "order_items"
+    __table_args__ = (
+        UniqueConstraint(
+            "order_id",
+            "product_id",
+            name="idx_unique_order_product",
+        ),
+    )
+    _order_back_populates = __tablename__
+    _product_back_populates = __tablename__
 
-    @declared_attr
-    def _product_back_populates(cls):
-        return TbHelper.generate_tn(cls)
-
-    quantity: Mapped[int] = mapped_column(nullable=False)
+    quantity: Mapped[int] = mapped_column(
+        nullable=False,
+        default=1,
+        server_default="1",
+    )
+    unit_price: Mapped[int]
