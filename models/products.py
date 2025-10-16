6ff748ff83435a017ca91f165746db0e67398b08
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-16 19:58:41 +0300

    Change product model

--- models/products.py
+++ models/products.py
@@ -1,13 +1,19 @@
 from typing import TYPE_CHECKING, List
 
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship, declared_attr
 from sqlalchemy import String, Integer, CheckConstraint
+
+import core.helpers
 from .base import Base
 from core.helpers import product_helper
 from core.mixins import TimestampMixin, CategoryRelationMixin
 
 if TYPE_CHECKING:
-    from models import OrderItem
+    from models import (
+        OrderItem,
+        ProductAttribute,
+        CartItem,
+    )
 
 
 class Product(TimestampMixin, CategoryRelationMixin, Base):
@@ -17,6 +23,10 @@ class Product(TimestampMixin, CategoryRelationMixin, Base):
             name="check_price_positive",
         ),
     )
+
+    @declared_attr
+    def _category_back_populates(cls):
+        return core.helpers.TbHelper.generate_tn(cls)
 
     name: Mapped[str] = mapped_column(
         String(product_helper.NAME_MAX_LEN),
@@ -38,4 +48,14 @@ class Product(TimestampMixin, CategoryRelationMixin, Base):
     order_items: Mapped[List["OrderItem"]] = relationship(
         "OrderItem",
         back_populates="product",
+    )
+
+    # Доступ к атрибутам (характеристикам): Название + Тип
+    product_attributes: Mapped[List["ProductAttribute"]] = relationship(
+        "ProductAttribute", back_populates="product"
+    )
+
+    # Доступ к данным: Пользователь + Количество
+    cart_items: Mapped[List["CartItem"]] = relationship(
+        "CartItem", back_populates="product"
     )
