de6d39ec5bce5e50338cf8d381de48d815f67117
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-16 20:15:28 +0300

    Min changes

--- routers/users/__init__.py
+++ routers/users/__init__.py
@@ -1,7 +1,5 @@
 __all__ = (
-    "pwd_helper",
     "UserCreate",
 )
 
-from .pwdHelper import pwd_helper
 from .schemas import UserCreate
--- routers/users/base.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Annotated
-
-
-class Base(BaseModel):
-    pass
