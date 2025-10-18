990d677dfd2872db73ba9a5d6d62812285b79d16
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-18 22:19:33 +0300

    Change user schemas

--- routers/users/schemas.py
+++ routers/users/schemas.py
@@ -6,31 +6,34 @@ from core.helpers import user_helper
 
 class UserBase(BaseModel):
     username: user_helper.UsernameStr
+
+
+class UserCreate(UserBase):
+    firstname: user_helper.FirstnameStr
+    lastname: user_helper.LastnameStr
+    email: user_helper.UserEmailStr
     password: Annotated[
         str,
         Field(min_length=1),
     ]
 
 
-class UserCreate(UserBase):
-    firstname: user_helper.FirstnameStr
-    surname: user_helper.SurnameStr
-    email: EmailStr
-
-
-class User(UserCreate):
+class User(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    firstname: user_helper.FirstnameStr
+    lastname: user_helper.LastnameStr
+    email: user_helper.UserEmailStr
     created_at: datetime
     updated_at: datetime
 
 
 class UserUpdate(UserBase):  # Partial update
     firstname: Optional[user_helper.FirstnameStr] = None
-    surname: Optional[user_helper.SurnameStr] = None
+    lastname: Optional[user_helper.LastnameStr] = None
     username: Optional[user_helper.UsernameStr] = None
-    email: Optional[EmailStr] = None
+    email: Optional[user_helper.UserEmailStr] = None
     password: Optional[Annotated[str, Field(min_length=1)]] = None
 
 
