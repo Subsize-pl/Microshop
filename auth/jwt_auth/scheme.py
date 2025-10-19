704446c1d0ad812b3caa363a60d5c5b76a53caab
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-19 13:31:25 +0300

    Change TokenInfo class for new refresh token functionality

--- auth/jwt_auth/scheme.py
+++ auth/jwt_auth/scheme.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class TokenInfo(BaseModel):
     access_token: str
+    refresh_token: Optional[str] = None
     content_type: str = "Bearer"
