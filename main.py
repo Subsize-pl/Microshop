7bc2ef74cb59a41f4b48f168fccd7755d40669c2
eduard plyushchevich <eduardatapovic@gmail.com>
2025-10-21 17:46:22 +0300

    Use ORJSONResponse as app default response class

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import asyncio
-
+from fastapi.responses import ORJSONResponse
 import uvicorn
 from fastapi import FastAPI
 from routers import products_router
 from auth import auth_router
 
-app = FastAPI()
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+)
 app.include_router(router=products_router)
 app.include_router(router=auth_router)
 
