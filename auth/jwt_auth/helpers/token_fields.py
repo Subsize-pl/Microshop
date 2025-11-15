12ae39e42d5373d91cf3e245bd640bbe424114bf
eduard plyushchevich <eduardatapovic@gmail.com>
2025-11-15 18:16:39 +0300

    Remove token field helper to token_field.py in helpers package

---
+++ auth/jwt_auth/helpers/token_fields.py
@@ -0,0 +1,9 @@
+class TokenFieldHelper:
+    SUB_FIELD = "sub"
+    TOKEN_TYPE_FIELD = "type"
+    JWT_ID_FIELD = "jti"
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+
+    ACCESS_TOKEN_TYPE = "access"
+    REFRESH_TOKEN_TYPE = "refresh"
