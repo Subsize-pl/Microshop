ff06e05dd3a7e8771795035ce9ba5dd4a143adb1
eduard plyushchevich <eduardatapovic@gmail.com>
2025-11-15 18:27:56 +0300

    Use prefix and tag helpers

--- auth/jwt_auth/routes.py
+++ auth/jwt_auth/routes.py
@@ -1,23 +1,32 @@
 from typing import Any, Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
-from .services import ensure_unique_user, get_curr_user_payload
-from fastapi import APIRouter, Depends
+from core.helpers.prefixes import Prefixes
+from core.helpers.tags import Tags
+from .services import ensure_unique_user  # , get_curr_user_payload
+from fastapi import APIRouter, Depends, status
 from routers.users.crud import create_user
 from core.helpers import db_helper
 from auth.jwt_auth.scheme import TokenInfo
 from routers.users.schemas import User, UserCreate
 from .services import validate_user_by_form
-from .helpers import token_creator
-from .dependencies import (
+from .helpers.token_creator import token_creator
+from .helpers.token_fields import TokenFieldHelper
+from .helpers.user_getter_by_token import (
     get_active_user_by_access_token,
     get_active_user_by_refresh_token,
-    token_fields,
 )
 
-jwt_auth_router = APIRouter(prefix="/jwt", tags=["Auth JWT"])
+
+router = APIRouter(
+    prefix=Prefixes.jwt_auth,
+    tags=[Tags.jwt_auth],
+)
 
 
-@jwt_auth_router.post("/login", response_model=TokenInfo)
+@router.post(
+    "/login",
+    response_model=TokenInfo,
+)
 async def issue_jwt(
     user: Annotated[User, Depends(validate_user_by_form)],
 ):
@@ -30,16 +39,19 @@ async def issue_jwt(
     )
 
 
-@jwt_auth_router.post("/refresh", response_model=TokenInfo)
+@router.post(
+    "/refresh",
+    response_model=TokenInfo,
+)
 async def refresh_access_token(
     user: Annotated[User, Depends(get_active_user_by_refresh_token)],
     # cached data -- no problem
-    payload: Annotated[dict, Depends(get_curr_user_payload)],
+    # payload: Annotated[dict, Depends(get_curr_user_payload)],
 ):
     access_token = token_creator.create_access_token(user)
     refresh_token = token_creator.create_refresh_token(user)
 
-    # Добавление старого токена в черный список
+    # TODO: implement adding the old token to the blacklist
 
     return TokenInfo(
         access_token=access_token,
@@ -47,7 +59,11 @@ async def refresh_access_token(
     )
 
 
-@jwt_auth_router.post("/register", response_model=User)
+@router.post(
+    "/register",
+    status_code=status.HTTP_201_CREATED,
+    response_model=User,
+)
 async def register_user(
     user_in: Annotated[UserCreate, Depends(ensure_unique_user)],
     session: Annotated[AsyncSession, Depends(db_helper.session_dependency)],
@@ -58,11 +74,14 @@ async def register_user(
     )
 
 
-@jwt_auth_router.get("/me")
+@router.get(
+    "/me",
+    response_model=dict[str, Any],
+)
 async def check_self_info(
     user: Annotated[User, Depends(get_active_user_by_access_token)],
-) -> dict[str, Any]:
+):
     return {
-        token_fields.USERNAME_FIELD: user.username,
-        token_fields.EMAIL_FIELD: user.email,
+        TokenFieldHelper.USERNAME_FIELD: user.username,
+        TokenFieldHelper.EMAIL_FIELD: user.email,
     }
