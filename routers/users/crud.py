6c6ed4af6311694c278013708aa4762194c7da20
eduard plyushchevich <eduardatapovic@gmail.com>
2025-11-15 18:21:17 +0300

    Extend user crud

--- routers/users/crud.py
+++ routers/users/crud.py
@@ -1,10 +1,28 @@
+from datetime import datetime, timezone
 from fastapi import HTTPException
-from typing import Optional
+from typing import Optional, List
+from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from models.users import User as UserORM
 from core.helpers import pwd_helper
-from routers.users.schemas import User, UserCreate
+from routers.users.schemas import User, UserCreate, UserUpdate, UserDelete
+
+
+async def get_user_by_id(
+    user_id: int,
+    session: AsyncSession,
+) -> Optional[User]:
+    return await session.get(UserORM, user_id)
+
+
+async def get_users(
+    session: AsyncSession,
+) -> List[User]:
+    stmt = select(UserORM).order_by(UserORM.id)
+    result: Result = await session.execute(stmt)
+    products = result.scalars().all()
+    return list(products)
 
 
 async def create_user(
@@ -19,14 +37,22 @@ async def create_user(
     return user
 
 
-async def get_user_by_id(
-    user_id: int,
+async def update_user(
+    user: User,
+    user_update: UserUpdate,
     session: AsyncSession,
-) -> Optional[User]:
-    if user := await session.get(UserORM, user_id):
-        return user
+) -> User:
+    for k, v in user_update.model_dump(exclude_unset=True).items():
+        setattr(user, k, v)
+    user.updated_at = datetime.now(tz=timezone.utc).replace(tzinfo=None)
+    await session.commit()
+    await session.refresh(user)
+    return user
 
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"User not found",
-    )
+
+async def delete_user(
+    user: UserDelete,
+    session: AsyncSession,
+) -> None:
+    await session.delete(user)
+    await session.commit()
